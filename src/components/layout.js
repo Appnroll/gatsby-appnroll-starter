51d625018efab21daac690173bd51df639bbc5a3
Mateusz Kocz <mateusz@kocz.pl>
2019-07-01 12:12:57 +0200

    Add App'n'roll as the author and remove unnecessary code

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -36,9 +35,9 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Made by
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://www.appnroll.com/">App'n'roll</a>
         </footer>
       </div>
     </>
